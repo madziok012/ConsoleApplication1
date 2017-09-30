d5f53e7be1c4271f8350a60b0300199f8377e3d0
Maciej Madzia <Maciej Madzia>
2017-09-30 19:55:03 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+// ConsoleApplication1.cpp: Definiuje punkt wejścia dla aplikacji konsolowej.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!!!!" << endl;
+	getchar();
+    return 0;
+}
+
